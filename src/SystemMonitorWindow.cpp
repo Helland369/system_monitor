73d43a4418ddd5d941b577266ec54e7ae5eaa2aa
Thomas Helland <thomas.helland97@gmail.com>
2025-03-05 15:36:12 +0100

    Made a window and cmakae + folder setup

---
+++ src/SystemMonitorWindow.cpp
@@ -0,0 +1,7 @@
+#include "include/SystemMonitorWindow.hpp"
+
+SystemMonitorWindow::SystemMonitorWindow()
+{
+  set_title("Basic application");
+  set_default_size(200, 200);
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "include/SystemMonitorWindow.hpp"
+#include <gtkmm-4.0/gtkmm/application.h>
+
+int main(int argc, char* argv[])
+{
+  auto app = Gtk::Application::create("basic.system.monitor");
+
+  return app->make_window_and_run<SystemMonitorWindow>(argc, argv);
+}
