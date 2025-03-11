c1032bdd22c07ccb2a968355ab434e61c7373542
Thomas Helland <thomas_helland@pm.me>
2025-03-11 15:00:00 +0100

    Cpu progress bar now works

--- src/SystemMonitorWindow.cpp
+++ src/SystemMonitorWindow.cpp
@@ -1,5 +1,10 @@
 #include "include/SystemMonitorWindow.hpp"
+#include "glibmm/main.h"
 #include "gtkmm/enums.h"
+#include <glibmm.h>
+#include <string>
+#include <thread>
+#include <chrono>
 
 SystemMonitorWindow::SystemMonitorWindow()
     : m_VBox(Gtk::Orientation::VERTICAL, 5),
@@ -26,7 +31,11 @@ SystemMonitorWindow::SystemMonitorWindow()
   m_progressbar_cpu.set_size_request(100, -1);
   m_progressbar_cpu.set_text("CPU progress bar");
   m_progressbar_cpu.set_show_text(true);
+  m_progressbar_cpu.get_pulse_step();
 
+  // update cpu progress bar
+  Glib::signal_timeout().connect([this]() { return update_cpu_progress_bar(); }, 1000);
+  
   m_frame_gpu.set_child(m_box_gpu);
   m_box_gpu.append(m_progressbar_gpu);
   m_progressbar_gpu.set_margin(5);
@@ -51,3 +60,17 @@ SystemMonitorWindow::SystemMonitorWindow()
 }
 
 SystemMonitorWindow::~SystemMonitorWindow() {}
+
+bool SystemMonitorWindow::update_cpu_progress_bar()
+{
+  prev = cpu.get_cpu_data();
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  curr = cpu.get_cpu_data();
+
+  double cpuUsage = cpu.calculate_cpu_usage(prev, curr);
+  
+  m_progressbar_cpu.set_fraction(cpuUsage / 100.0);
+  m_progressbar_cpu.set_text(std::to_string(cpuUsage) + "%");
+
+  return true; // to keep the time out active
+}
--- src/include/SystemMonitorWindow.hpp
+++ src/include/SystemMonitorWindow.hpp
@@ -5,14 +5,23 @@
 #include <gtkmm-4.0/gtkmm/box.h>
 #include <gtkmm-4.0/gtkmm/progressbar.h>
 
+#include "CpuUsage.hpp"
+
 class SystemMonitorWindow : public Gtk::Window
 {
-protected:
+private:
   Gtk::Box m_HBox;
   Gtk::Box m_VBox;
   Gtk::Box m_box_cpu, m_box_gpu, m_box_ram;
   Gtk::Frame m_frame_cpu, m_frame_gpu, m_frame_ram;
   Gtk::ProgressBar m_progressbar_cpu, m_progressbar_gpu, m_progressbar_ram;
+
+  // CPU
+  CpuUsage cpu;
+  CpuData prev; 
+  CpuData curr;
+
+  bool update_cpu_progress_bar();
 
 public:
   SystemMonitorWindow();
