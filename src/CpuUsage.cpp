024b71fafb4f0d99f5253e7ef33bb84eb997df92
Thomas Helland <thomas_helland@pm.me>
2025-03-12 14:21:37 +0100

    Working on getting thread info and making the code simpler

--- src/CpuUsage.cpp
+++ src/CpuUsage.cpp
@@ -2,7 +2,6 @@
 
 #include <fstream>
 #include <sstream>
-#include <string>
 
 CpuData CpuUsage::get_cpu_data()
 {
@@ -10,7 +9,8 @@ CpuData CpuUsage::get_cpu_data()
   std::string line; // store some thing like this; cpu 1074181 43 264124 24623195 9219 96703 47641 0 0 0 // messured in jiffies
   CpuData data;
 
-  if (file.is_open()) {
+  if (file.is_open())
+  {
     std::getline(file, line);    // read file
     std::istringstream ss(line); // input string stream from line, this make it possible to break it down.
     std::string cpu; // store the "cpu" part we get from /proc/stat
@@ -18,7 +18,7 @@ CpuData CpuUsage::get_cpu_data()
     ss >> cpu >> data.user >> data.nice >> data.system >> data.idle >>
           data.iowait >> data.irq >> data.softirq >> data.steal;
   }
-  // return struct
+
   return data;
 }
 
@@ -35,4 +35,33 @@ double CpuUsage::calculate_cpu_usage(const CpuData &prev, const CpuData &curr)
   double idleDelta = currIdle - prevIdle;
 
   return (1.0 - (idleDelta / totalDelta)) * 100.0;
-}    
+}
+
+// TODO use this instead of the above functions
+std::vector<CpuData> CpuUsage::get_cpu_tread_data()
+{
+  std::ifstream file("/proc/stat");
+  std::string line;
+  std::vector<CpuData> cpu_data;
+
+  if (file.is_open())
+  {
+    while (std::getline(file, line)) {
+      
+      std::istringstream ss(line);
+      //std::string thread;
+      CpuData data;
+
+      ss >> data.label >> data.user >> data.nice >> data.system >> data.idle >> data.iowait >> data.irq >> data.softirq >> data.steal;
+
+      if (data.label.find("cpu") != std::string::npos)
+      {
+        cpu_data.push_back(data);
+      }
+    }
+  }
+
+  return cpu_data;
+}
+
+//double CpuUsage::calculate_cpu_thread_usage(const CpuData &prev, const CpuData &curr) {}
--- src/include/CpuUsage.hpp
+++ src/include/CpuUsage.hpp
@@ -1,18 +1,28 @@
 #pragma once
 
-class CpuData {
+#include <string>
+#include <vector>
+
+
+class CpuData
+{
 public:
-    unsigned long long user, nice, system, idle, iowait, irq, softirq,
+  std::string label;
+  unsigned long long user, nice, system, idle, iowait, irq, softirq,
         steal;
 };
 
-class CpuUsage {
+class CpuUsage
+{
 
 private:
 
 public:
   CpuData get_cpu_data();
 
+  std::vector<CpuData> get_cpu_tread_data();
+  
   double calculate_cpu_usage(const CpuData &prev, const CpuData &curr);
 
+  double calculate_cpu_thread_usage(const CpuData &prev, const CpuData &curr);
 };
