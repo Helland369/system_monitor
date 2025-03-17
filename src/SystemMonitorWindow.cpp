d9a7af42639dfb6645e386a08390d0e0012991d0
Thomas Helland <thomas_helland@pm.me>
2025-03-17 14:24:12 +0100

    added some css and css error handling

--- src/SystemMonitorWindow.cpp
+++ src/SystemMonitorWindow.cpp
@@ -1,11 +1,18 @@
 #include "include/SystemMonitorWindow.hpp"
 #include "CpuUsage.hpp"
+#include "glibmm/error.h"
 #include "glibmm/main.h"
+#include "glibmm/refptr.h"
+#include "gtk/gtk.h"
+#include "gtkmm/csssection.h"
 #include "gtkmm/enums.h"
 #include "gtkmm/object.h"
 #include "gtkmm/progressbar.h"
+#include "gtkmm/stylecontext.h"
+#include <gtkmm/cssprovider.h>
 #include <cstddef>
 #include <glibmm.h>
+#include <iostream>
 #include <string>
 #include <thread>
 #include <chrono>
@@ -22,7 +29,7 @@ SystemMonitorWindow::SystemMonitorWindow()
   set_title("System Monitor");
   // set window size
   set_default_size(800, 800);
-
+  
   m_HBox.set_margin(5);
   set_child(m_HBox);
   m_HBox.append(m_VBox);
@@ -52,9 +59,27 @@ SystemMonitorWindow::SystemMonitorWindow()
     {
       double usageFraction = cpuUsageData[i] / 100.0;
       m_progressbar_cpu[i]->set_fraction(usageFraction);
-      m_progressbar_cpu[i]->set_text("CPU" + std::to_string(i) + " " + std::to_string(cpuUsageData[i]) + "%");
+      m_progressbar_cpu[i]->add_css_class("cpu-progress-bar");
+      m_progressbar_cpu[i]->set_size_request(300, -1);
+      m_progressbar_cpu[i]->set_vexpand(true);
+      m_progressbar_cpu[i]->set_hexpand(true);
+
+      if (i == 0)
+      {
+        m_progressbar_cpu[0]->set_text("Total CPU " + std::to_string(cpuUsageData[0]) + "%");
+      }
+      else
+      {
+        m_progressbar_cpu[i]->set_text("CPU" + std::to_string(i) + " " + std::to_string(cpuUsageData[i]) + "%");
+      }
     }
   });
+
+  // allow cpu frame to expand
+  m_frame_cpu.set_hexpand(true);
+  m_frame_cpu.set_valign(Gtk::Align::FILL);
+  // set cpu css class
+  m_frame_cpu.add_css_class("cpu-frame");
   
   // update cpu progress bar
   Glib::signal_timeout().connect([this]() { return update_cpu_progress_bar(); }, 1000);
@@ -76,10 +101,24 @@ SystemMonitorWindow::SystemMonitorWindow()
   m_progressbar_ram.set_size_request(100, -1);
   m_progressbar_ram.set_text("RAM progress bar");
   m_progressbar_ram.set_show_text(true);
-
+  
   m_VBox.append(m_frame_cpu);
   m_VBox.append(m_frame_gpu);
   m_VBox.append(m_frame_ram);
+
+  m_ref_css_provider = Gtk::CssProvider::create();
+#if HAS_STYLE_PROVIDER_ADD_PROVIDER_FOR_DISPLAY
+  Gtk::StyleProvider::add_provider_for_display(get_display(), m_ref_css_provider,
+                                               GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+#else
+  Gtk::StyleContext::add_provider_for_display(get_display(), m_ref_css_provider,
+                                              GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+#endif
+
+  m_ref_css_provider->signal_parsing_error().connect(
+      [](const auto &section, const auto &err)
+        { on_parsing_error(section, err); });
+  m_ref_css_provider->load_from_path("../css/style.css");
 }
 
 SystemMonitorWindow::~SystemMonitorWindow() {}
@@ -104,4 +143,26 @@ bool SystemMonitorWindow::update_cpu_progress_bar()
   }).detach();
 
   return true;
+}
+
+void SystemMonitorWindow::on_parsing_error(const Glib::RefPtr<const Gtk::CssSection>& section, const Glib::Error & error)
+{
+  std::cerr << "on_parsing_error(): " << error.what() << "\n";
+  if (section)
+  {
+    const auto file = section->get_file();
+    if (file)
+    {
+      std::cerr << "  URI = " << file->get_uri() << "\n";
+    }
+
+    auto start_location = section->get_start_location();
+    auto end_location = section->get_end_location();
+
+    std::cerr << "  start_line = " << start_location.get_lines()+1
+    << ", end_line = " << end_location.get_lines()+1 << "\n";
+
+    std::cerr << "  start_position = " << start_location.get_line_chars()
+    << ", end_position = " << end_location.get_line_chars() << "\n";
+  }
 }
