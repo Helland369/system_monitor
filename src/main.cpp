c53402ea57542d3159ebbc230f98da510fa69670
Thomas Helland <thomas_helland@pm.me>
2025-03-20 14:50:40 +0100

    Can now show nvidia gpu info in progressbar, but there are bugs..

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include "include/SystemMonitorWindow.hpp"
 #include <gtkmm-4.0/gtkmm/application.h>
-#include "include/NvidiaInfo.hpp"
 
 int main(int argc, char* argv[])
 {
   auto app = Gtk::Application::create("basic.system.monitor");
-
-  NvidiaInfo n;
-  n.printNvml();
   
   return app->make_window_and_run<SystemMonitorWindow>(argc, argv);
 }
